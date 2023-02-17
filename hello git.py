a9aa751b9b5e0917c792b11423afd7ae7c8f7251
Ratchaneei <125616592+Ratchaneei@users.noreply.github.com>
2023-02-17 07:22:58 +0700

    Add files via upload

---
+++ hello git.py
@@ -0,0 +1,23 @@
+print('helo world')
+print('helo world')
+print('helo world')
+
+
+
+print('helo world')
+print('helo world')
+print('helo world')
+
+
+print('helo world')
+print('helo world')
+print('helo world')
+
+print('hello Github')
+friend = ['loong' , 'dekdek' , 'PP']
+print friend
+
+
+
+
+
